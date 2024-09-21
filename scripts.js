31e8948ce58b66be69c774171ac458e7dbbd278f
Mohamed Hossam <152012615+MohamedHossam606@users.noreply.github.com>
2024-09-21 16:47:32 +0300

    Add files via upload
    ST

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.navbar-nav .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+});
